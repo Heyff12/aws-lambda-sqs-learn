1a41a9d7d8a9179b0fb5541b2ddf32af1bdd260f
junfang chu <yaya@junfangdeMacBook-Pro.local>
2021-10-17 23:43:06 +0800

    create bucket

--- lib/lambda-sqs-stack.ts
+++ lib/lambda-sqs-stack.ts
@@ -1,9 +1,18 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class LambdaSqsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create bucket
+    const bucket = new Bucket(this, 'LambdaSqsBucket',{
+      encryption: BucketEncryption.S3_MANAGED
+    })
+
+    new cdk.CfnOutput(this,'LambdaSqsBucketNameExport',{
+      value: bucket.bucketName,
+      exportName: 'LambdaSqsBucketName'
+    })
   }
 }
