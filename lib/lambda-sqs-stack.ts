a603db4e20875f1cfd12024f86a077ccebfac080
junfang chu <yaya@junfangdeMacBook-Pro.local>
2021-10-17 23:59:39 +0800

    create lambda

--- lib/lambda-sqs-stack.ts
+++ lib/lambda-sqs-stack.ts
@@ -1,5 +1,8 @@
 import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs'
+import { Runtime } from '@aws-cdk/aws-lambda';
+import * as path from 'path';
 
 export class LambdaSqsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +13,17 @@ export class LambdaSqsStack extends cdk.Stack {
       encryption: BucketEncryption.S3_MANAGED
     })
 
+    // create lambda getPhotos, handler is in folder named api
+    const getPhotos = new lambda.NodejsFunction(this,'LambdaSqsLambda',{
+      runtime: Runtime.NODEJS_12_X,
+      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      handler: 'getPhotos',
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName
+      }
+    })
+
+    // output values
     new cdk.CfnOutput(this,'LambdaSqsBucketNameExport',{
       value: bucket.bucketName,
       exportName: 'LambdaSqsBucketName'
