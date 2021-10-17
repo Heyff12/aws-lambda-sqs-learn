4768904a08ec99d6d9b02994bc337f2016be8bcd
junfang chu <yaya@junfangdeMacBook-Pro.local>
2021-10-18 00:13:13 +0800

    deploy phptos to bucket

--- lib/lambda-sqs-stack.ts
+++ lib/lambda-sqs-stack.ts
@@ -3,14 +3,22 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs'
 import { Runtime } from '@aws-cdk/aws-lambda';
 import * as path from 'path';
+import {BucketDeployment, Source} from '@aws-cdk/aws-s3-deployment'
 
 export class LambdaSqsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // create bucket
+    // create bucket  and deploy photos to bucket 
     const bucket = new Bucket(this, 'LambdaSqsBucket',{
       encryption: BucketEncryption.S3_MANAGED
+    })
+
+    new BucketDeployment(this, 'LambdaSqsPhotos', {
+      sources: [
+        Source.asset(path.join(__dirname,'..','photos'))
+      ],
+      destinationBucket: bucket
     })
 
     // create lambda getPhotos, handler is in folder named api
